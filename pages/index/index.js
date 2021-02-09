f3b9412d8c7fe9f138df76fa3702be1338c70302
Chen zeng <1476104805@qq.com>
2021-02-09 20:13:35 +0800

    swiper

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,38 +1,20 @@
 //Page Object
 Page({
   data: {
-    
+    //轮播图数组
+    swiperList:[],
   },
   //options(Object)
   onLoad: function(options){
-    
+    //通过异步请求获取轮播图数据
+    var reqTask = wx.request({
+      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
+      success: (result)=>{
+        console.log(result);
+        this.setData({
+          swiperList:result.data.message,
+        })
+      }
+    });
   },
-  onReady: function(){
-    
-  },
-  onShow: function(){
-    
-  },
-  onHide: function(){
-
-  },
-  onUnload: function(){
-
-  },
-  onPullDownRefresh: function(){
-
-  },
-  onReachBottom: function(){
-
-  },
-  onShareAppMessage: function(){
-
-  },
-  onPageScroll: function(){
-
-  },
-  //item(index,pagePath,text)
-  onTabItemTap:function(item){
-
-  }
 });
