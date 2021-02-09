f088ed5d6cf49372ae093d8be5a1d531fbbb4463
Chen zeng <1476104805@qq.com>
2021-02-09 20:57:00 +0800

    Fixed bug MC-115716 - Real machine debugging does not display: API problem

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,17 +2,22 @@
 Page({
   data: {
     //轮播图数组
-    swiperList:[],
+    swiperList:[
+      "https://img.imgdb.cn/item/602284a73ffa7d37b33b1417.png",
+      "https://img.imgdb.cn/item/602285703ffa7d37b33b719c.png",
+      "https://img.imgdb.cn/item/6022858d3ffa7d37b33b8034.png"
+    ],
   },
   //options(Object)
   onLoad: function(options){
-    //通过异步请求获取轮播图数据
+    //通过异步请求获取轮播图数据,由于这些数据在手机上无法使用所以这里也就只是用于学习
     var reqTask = wx.request({
       url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
       success: (result)=>{
         console.log(result);
         this.setData({
-          swiperList:result.data.message,
+          // 防止修改
+          // swiperList:result.data.message,
         })
       }
     });
