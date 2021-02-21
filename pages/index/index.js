503210074b8d814955e4a0bda97fdde3f0272e12
Chen zeng <1476104805@qq.com>
2021-02-21 21:58:07 +0800

    1. Home page completed 2, search box top optimization 2. Complete the static design of the classification page

--- pages/index/index.js
+++ pages/index/index.js
@@ -11,17 +11,50 @@ Page({
       "https://img.imgdb.cn/item/602285703ffa7d37b33b719c.png",
       "https://img.imgdb.cn/item/6022858d3ffa7d37b33b8034.png"
     ],
+    catesList:[],
+    floorList:[]
   },
   //options(Object)
   onLoad: function (options) {
-    //通过异步请求获取轮播图数据,由于这些数据在手机上无法使用所以这里也就只是用于学习
+    
+    this.getSwiperList();
+    this.getCateList();
+    this.getFloorList();
+    
+  },
+
+  //通过异步请求获取轮播图数据,由于这些数据在手机上无法使用所以这里也就只是用于学习
+  getSwiperList(){
     request({
-        url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata'
+      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata'
+    })
+    .then(result => {
+      this.setData({
+        swiperList: result.data.message
       })
-      .then(result => {
-        this.setData({
-          swiperList: result.data.message
-        })
+    })
+  },
+
+  // 获取分类导航标签数据
+  getCateList(){
+    request({
+      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/catitems'
+    })
+    .then(result => {
+      this.setData({
+        catesList: result.data.message
       })
+    })
+  },
+
+  getFloorList(){
+    request({
+      url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/floordata'
+    })
+    .then(result => {
+      this.setData({
+        floorList: result.data.message
+      })
+    })
   },
 });
